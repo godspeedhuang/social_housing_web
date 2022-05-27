87bd9279c42feb0061926a63c857da9bbd6101e7
godspeed88168 <godspeed88168@gmail.com>
2022-05-28 01:06:10 +0800

    version2

--- app.py
+++ app.py
@@ -3,11 +3,10 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import redirect
-from flask import send_file
 
 # insert map
 import folium
+from numpy import NaN
 # import sql
 from sqlalchemy import create_engine
 # import geo_tool
@@ -15,7 +14,6 @@ import pyproj
 import geopandas as gpd
 #import pkg
 import map_set
-
 import time
 
 app = Flask(__name__, static_folder='static', static_url_path='/')
@@ -25,7 +23,7 @@ app = Flask(__name__, static_folder='static', static_url_path='/')
 @app.route('/')
 def index():
     map=map_set.create_map()
-    map_set.add_data('高雄市社會住宅點位',map,engine)
+    map_set.add_data_from_sql('高雄市社會住宅點位',map,engine)
     map.save(r"web\templates\map.html")
     return render_template('homepage.html')
 
@@ -33,34 +31,66 @@ def index():
 @app.route('/search')
 def get_results():
     name = request.args.get('social_housing', '')
-    data=map_set.search('高雄市社會住宅點位',name,engine)
+    data=map_set.search('高雄市社會住宅點位','名稱',name,engine)
     households_num=data['總戶數'].values[0]
+    global one_room,two_room,three_room,housing_population
     one_room=data['一房型'].values[0]
     two_room=data['二房型'].values[0]
     three_room=data['三房型'].values[0]
     organizer=data['單位'].values[0]
-    map2=map_set.create_map()
+    
+    map=map_set.create_map()
+    # 匯入社宅點位資料
     folium.GeoJson(
         data.to_json()
-    ).add_to(map2)
+    ).add_to(map)
     
-    # add buffer
+    # 社宅點位buffer500公尺
+    global buffer
     data=data.to_crs(epsg=3826)
     buffer=data.buffer(500)
     buffer=buffer.to_crs(epsg=4326)
     folium.GeoJson(
         buffer.to_json()
-    ).add_to(map2)
+    ).add_to(map)
     
-    map2.fit_bounds(map2.get_bounds(),padding=(30,30))
-    map2.save(r"web\templates\map.html")
-    return render_template('results.html',
+    # zoom in 到社宅及buffer點位
+    map.fit_bounds(map.get_bounds(),padding=(30,30))
+    map.save(r"web\templates\map.html")
+
+    if one_room>0 and two_room>0 and three_room>0:
+        one_room=int(one_room);two_room=int(two_room);three_room=int(three_room)
+        housing_population=one_room+two_room*3+three_room*4
+    else:
+        housing_population=False;one_room=False;two_room=False;three_room=False
+    return render_template('search.html',
                             name=name,
-                            households_num=households_num,
+                            households_num=int(households_num),
                             one_room=one_room,
                             two_room=two_room,
                             three_room=three_room,
+                            housing_population=housing_population,
                             organizer=organizer)
+
+@app.route('/analysis')
+def analysis():
+    one=int(request.args.get('one',one_room))
+    two=int(request.args.get('two',two_room))
+    three=int(request.args.get('three',three_room))
+    sum=one+two*3+three*4
+    
+    # 根據計畫容納人口判斷提供的公設項目
+    if sum<300:
+        # 未滿300人不提供托育服務
+        kid=False
+    elif sum<1500:
+        # 未滿1500人不提供青年創業空間
+        startup=False
+    
+    # buffer500覆蓋到的行政區
+    vill=map_set.get_data('高雄市村里界',engine=engine)
+
+    return render_template('analysis.html',)
 
 @app.route('/map')
 def map():
@@ -69,6 +99,8 @@ def map():
 @app.route('/spatial_set')
 def set():
     return render_template('spatial_set.html')
+
+
 
 if __name__ == '__main__':
     pyproj.datadir.set_data_dir('C:\\Users\\godsp\\anaconda3\\envs\\geo_env\\Library\\share\\proj')
--- map_set.py
+++ map_set.py
@@ -4,8 +4,8 @@ import geopandas as gpd
 import json
 import pyproj
 
-# 從資料庫裡撈data
 def get_data(data_name,engine):
+    """從資料庫裡撈data，data_name是資料庫裡的table名稱，engine是串接資料庫的接口"""
     sql=f'select * from public.{data_name}'
     data_file=gpd.GeoDataFrame.from_postgis(sql,engine,geom_col='geom')
     # data_file.crs=4326
@@ -18,30 +18,30 @@ def swap_coordinate(data):
         d['geometry']['coordinates'][0],d['geometry']['coordinates'][1]=d['geometry']['coordinates'][1],d['geometry']['coordinates'][0]
     return new_data
 
-# 創建地圖
+# 創建初始化地圖
 def create_map(coordinate=(22.62516116466572, 120.29979192527767)):
     map=folium.Map(coordinate,tiles="Cartodb Positron",zoom_start=12,control_scale=True)
     return map
 
-# 增加geojson資料
-def add_data(data_name,map_name,engine):
+
+def add_data_from_sql(data_name,map_name,engine):
+    """從資料庫中撈data轉成geojson資料匯入地圖上"""
     folium.GeoJson(
         get_data(data_name,engine).to_json(),
-        name=data_name,
-        popup="test"
+        name=data_name
     ).add_to(map_name)
 
 
-def search(data_name,search_name,engine):
+def search(data_name,filter,search_name,engine):
+    """從資料庫中撈data，並回傳資料"""
     data=get_data(data_name,engine)
-    data=data[data['名稱']==search_name]
+    data=data[data[filter]==search_name]
     return data
 
 if __name__=='__main__':
     pyproj.datadir.set_data_dir('C:\\Users\\godsp\\anaconda3\\envs\\geo_env\\Library\\share\\proj')
     engine=create_engine('postgresql://postgres:5733@localhost:5432/testing')
     map=create_map()
-    add_data('高雄市私立托嬰中心名冊',map)
     map.save(r"web\templates\map.html")
     
 
